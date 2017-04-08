9a2c0ce2e9773a59505e8cb98213a3d0f423b4d1
Patrick <dummy>
2017-04-07 22:25:12 -0400

    Cleaning up color training script.

--- tf/train-color.py
+++ tf/train-color.py
@@ -11,14 +11,11 @@ import tensorflow as tf
 
 from six.moves import cPickle as pickle
 
-PIXEL_DEPTH = 255.0
-TRAINING_DIR = "./training"
-ORGANIZED_DIR = "./organized"
-RAW_IMAGE_SHAPE = (150, 250, 3) #RBG channel of a landscape oriented card
+PIXEL_DEPTH         = 255.0
+TRAINING_DIR        = "./training"
+RAW_IMAGE_SHAPE     = (150, 250, 3) #RBG channel of a landscape oriented card
 REDUCED_IMAGE_SHAPE = (30, 50, 3)
-
-files = os.listdir(TRAINING_DIR)
-print "files found:", len(files)
+NUM_COLORS          = 3
 
 def getFileColor(s):
     # assumes color is the first token of the file name
@@ -39,93 +36,72 @@ def organizeByColor(files):
 def normalizeImage(img):
     return (img-PIXEL_DEPTH/2)/PIXEL_DEPTH
 
-def pickleColor(organized, color):
-    files = organized[color]
-    dataset = np.ndarray(shape=(len(files),) + REDUCED_IMAGE_SHAPE,
-                         dtype=np.float32)
-    for i in xrange(len(files)):
-        f = os.path.join(TRAINING_DIR, files[i])
-        raw = ndimage.imread(f).astype(float)
-        reduced = scipy.misc.imresize(raw, REDUCED_IMAGE_SHAPE)
-        dataset[i, :, :, :] = normalizeImage(reduced)
-
-    print color, "mean:", np.mean(dataset)
-    print color, "std:", np.std(dataset)
-        
-    print color, "dataset shape:", dataset.shape
-    outputFile = os.path.join(ORGANIZED_DIR, color + ".pickle")
-    with open(outputFile, 'wb') as f:
-          pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
-
-def splitDatasets():
-    # 80/10/10 for training/validation/test sets
-    rtnTrainingData   = np.ndarray(shape=(0,)+REDUCED_IMAGE_SHAPE, dtype=np.float32)
-    rtnValidationData = np.ndarray(shape=(0,)+REDUCED_IMAGE_SHAPE, dtype=np.float32)
-    rtnTestData       = np.ndarray(shape=(0,)+REDUCED_IMAGE_SHAPE, dtype=np.float32)
-
-    rtnTrainingLabels   = np.ndarray(shape=(0), dtype=np.int32)
-    rtnValidationLabels = np.ndarray(shape=(0), dtype=np.int32)
-    rtnTestLabels       = np.ndarray(shape=(0), dtype=np.int32)
-    
-    for _, (color, label) in enumerate([("RED", 0), ("PURPLE", 1), ("GREEN", 2)]):
-        with open(os.path.join(ORGANIZED_DIR, color + ".pickle"), 'rb') as f:
-            data = pickle.load(f)
-            np.random.shuffle(data)
-
-            trainingSize = int(0.8*data.shape[0])
-            rtnTrainingData = np.concatenate([rtnTrainingData,
-                                              data[0:trainingSize, :, :, :]])
-            rtnTrainingLabels = np.concatenate([rtnTrainingLabels, np.full(trainingSize,
-                                                                           label,
-                                                                           dtype=np.int32)])
-            
-            validationSize = int(0.1*data.shape[0])
-            rtnValidationData = np.concatenate([rtnValidationData,
-                                                data[trainingSize:trainingSize+validationSize, :, :, :]])
-            rtnValidationLabels = np.concatenate([rtnValidationLabels,
-                                                  np.full(validationSize,
-                                                          label,
-                                                          dtype=np.int32)])
-
-            testSize = data.shape[0]-trainingSize-validationSize
-            rtnTestData = np.concatenate([rtnTestData,
-                                          data[trainingSize+validationSize:, :, :, :]])
-            rtnTestLabels = np.concatenate([rtnTestLabels,
-                                            np.full(testSize,
-                                                    label,
-                                                    dtype=np.int32)])
-
-    return rtnTrainingData, rtnValidationData, rtnTestData, rtnTrainingLabels, rtnValidationLabels, rtnTestLabels
-
-def nextTrainingBatch(data, labels, batchSize):
-    indices = random.sample(range(data.shape[0]), batchSize)
-    return {
-        "data" : data[indices, :],
-        "labels" : labels[indices],
+def loadAndNormalize(organizedFiles):
+    classification = {
+        "RED" : 0,
+        "PURPLE" : 1,
+        "GREEN" : 2,
     }
 
+    features = np.ndarray(shape=(len(files),) + REDUCED_IMAGE_SHAPE,
+                      dtype=np.float32)
+    labels = np.ndarray(shape=(len(files),), dtype=np.int32)
+
+    i = 0
+    for color in organizedFiles.iterkeys():
+        for f in organizedFiles[color]:
+            f = os.path.join(TRAINING_DIR, f)
+            raw = ndimage.imread(f).astype(float)
+            reduced = scipy.misc.imresize(raw, REDUCED_IMAGE_SHAPE)
+            features[i, :, :, :] = normalizeImage(reduced)
+            labels[i] = classification[color]
+            i += 1
+    return features, labels
+
+def splitData(features, labels):
+    indices = np.random.permutation(features.shape[0])
+
+    # 80/10/10 training/validation/test split
+    trainingSize = int(len(indices)*0.8)
+    validationSize = int(len(indices)*0.1)
+    testSize = len(indices)-trainingSize-validationSize
+
+    rtn = {
+        "training" : {
+            "features" : features[indices[:trainingSize], :, :, :],
+            "labels"   : labels[indices[:trainingSize]],
+        },
+        "validation" : {
+            "features" : features[indices[trainingSize:trainingSize+validationSize], :, :, :],
+            "labels"   : labels[indices[trainingSize:trainingSize+validationSize]],
+        },
+        "test" : {
+            "features" : features[indices[trainingSize+validationSize:], :, :, :],
+            "labels"   : labels[indices[trainingSize+validationSize:]],
+        },
+    }
+
+    return rtn
+
+# Loading the features
+files = os.listdir(TRAINING_DIR)
+print "files found:", len(files)
 organized = organizeByColor(files)
-print "RED count:", len(organized["RED"])
-print "PURPLE count:", len(organized["PURPLE"])
-print "GREEN count:", len(organized["GREEN"])
+features, labels = loadAndNormalize(organized)
+split = splitData(features, labels)
 
-pickleColor(organized, "RED")
-pickleColor(organized, "PURPLE")
-pickleColor(organized, "GREEN")
+def nextTrainingBatch(data, batchSize):
+    indices = random.sample(range(data["features"].shape[0]), batchSize)
+    return {
+        "features" : data["features"][indices, :],
+        "labels"   : data["labels"][indices],
+    }
 
-trainingData, validationData, testData, trainingLabels, validationLabels, testLabels = splitDatasets()
-print trainingData.shape, validationData.shape, testData.shape
-print trainingLabels.shape, validationLabels.shape, testLabels.shape
+def formatFeatures(features):
+    return features.reshape((-1, np.prod(REDUCED_IMAGE_SHAPE))).astype(np.float32)
 
-def reformat(dataset, labels):
-  dataset = dataset.reshape((-1, np.prod(REDUCED_IMAGE_SHAPE))).astype(np.float32)
-  # Map 0 to [1.0, 0.0, 0.0 ...], 1 to [0.0, 1.0, 0.0 ...]
-  labels = (np.arange(3) == labels[:,None]).astype(np.float32)
-  return dataset, labels
-
-train_dataset, train_labels = reformat(trainingData, trainingLabels)
-valid_dataset, valid_labels = reformat(validationData, validationLabels)
-test_dataset, test_labels   = reformat(testData, testLabels)
+def formatLabels(labels):
+    return np.arange(NUM_COLORS) == labels[:,None].astype(np.float32)
 
 # Train model
 session = tf.InteractiveSession()
@@ -149,17 +125,26 @@ optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
 for i in range(500):
-    batch = nextTrainingBatch(train_dataset, train_labels, 200)
-    optimizer.run(feed_dict={x: batch["data"], y_: batch["labels"]})
+    batch = nextTrainingBatch(split["training"], 200)
+    optimizer.run(feed_dict={
+        x  : formatFeatures(batch["features"]),
+        y_ : formatLabels(batch["labels"]),
+    })
     if 0 == i%100:
         print 'checkpoint accuracy %g' % session.run(
-            accuracy, feed_dict={x: valid_dataset,
-                                 y_: valid_labels})                  
+            accuracy, feed_dict={
+                x  : formatFeatures(split["validation"]["features"]),
+                y_ : formatLabels(split["validation"]["labels"]),
+            })                  
 
 print 'training accuracy %g' % session.run(
-    accuracy, feed_dict={x: test_dataset,
-                         y_: test_labels})
+    accuracy, feed_dict={
+        x  : formatFeatures(split["test"]["features"]),
+        y_ : formatLabels(split["test"]["labels"]),
+    })
 
+
+# Start the classification server
 import cv2
 class MyTCPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
@@ -173,8 +158,6 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
         )
         self.request.sendall(["RED", "PURPLE","GREEN"][prediction])
 
-if __name__ == "__main__":
-    server = SocketServer.TCPServer(("localhost", 9000), MyTCPHandler)
-
-    print "starting model service"
-    server.serve_forever()
+server = SocketServer.TCPServer(("localhost", 9000), MyTCPHandler)
+print "starting model service"
+server.serve_forever()
